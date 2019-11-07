7361dedd1dca33c52521087ddd8e46abf40eb772
Aenik Nur Aufa <aeniknuraufa@users.noreply.github.com>
2019-11-07 22:05:47 +0700

    PBO2-10118082-Latihan51
    Membuat program berbasis objek untuk menghitung gaji karyawan

---
+++ README.md
@@ -0,0 +1,13 @@
+# PBO2-10118052-Latihan51-GajiKaryawan
+
+```java
+/**
+ *	
+ * @author
+ * NAMA     : Bagus Syibro Malisi
+ * KELAS    : IF-2
+ * NIM      : 10118052
+ * Deskripsi Program : Membuat program berbasis objek untuk menghitung gaji
+ * karyawan
+ */
+ ```
---
+++ src/pbo2/pkg10118082/latihan51/gajikaryawan/Karyawan.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118082.latihan51.gajikaryawan;
+
+/**
+ *	
+ * @author
+ * Nama     : Aenik Nur Aufa
+ * Kelas    : IF 2
+ * NIM      : 10118082
+ * Deskripsi Program : Membuat program berbasis objek untuk menghitung gaji
+ * karyawan
+ */
+public class Karyawan {
+    protected String nik,nama,jabatan;
+    protected int golongan;
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public int getGolongan() {
+        return golongan;
+    }
+
+    public void setGolongan(int golongan) {
+        this.golongan = golongan;
+    }
+    
+}
